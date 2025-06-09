f575193994614dfc6b60a7f4dfedaee190f1f709
github-ci <you@example.com>
2025-06-09 21:56:46 +0800

    init

---
+++ src/IntegrationTestKernel.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Tourze\IntegrationTestKernel;
+
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use Symfony\Component\Security\Core\User\InMemoryUser;
+use Tourze\BundleDependency\ResolveHelper;
+
+class IntegrationTestKernel extends BaseKernel
+{
+    public function __construct(string $environment, bool $debug, private readonly array $appendBundles = [])
+    {
+        parent::__construct($environment, $debug);
+    }
+
+    public function registerBundles(): iterable
+    {
+        $baseBundles = array_merge([
+            FrameworkBundle::class => ['all' => true],
+        ], $this->appendBundles);
+
+        foreach (ResolveHelper::resolveBundleDependencies($baseBundles) as $bundle => $env) {
+            yield new $bundle();
+        }
+    }
+
+    public function getCacheDir(): string
+    {
+        return sys_get_temp_dir() . '/var/cache/' . $this->environment;
+    }
+
+    public function getLogDir(): string
+    {
+        return sys_get_temp_dir() . '/var/log';
+    }
+
+    private function configureContainer(ContainerBuilder $container): void
+    {
+        // 基础框架
+        if ($container->hasExtension('framework')) {
+            $container->prependExtensionConfig('framework', [
+                'secret' => 'TEST_SECRET',
+                'test' => true,
+                'http_method_override' => false,
+                'handle_all_throwables' => true,
+                'php_errors' => [
+                    'log' => true,
+                ],
+                'validation' => [
+                    'email_validation_mode' => 'html5',
+                ],
+                'uid' => [
+                    'default_uuid_version' => 7,
+                    'time_based_uuid_version' => 7,
+                ],
+            ]);
+        }
+
+        // EasyAdmin需要开启路由
+        if ($container->hasExtension('easy_admin')) {
+            $container->prependExtensionConfig('framework', [
+                'router' => [
+                    'resource' => __DIR__ . '/../config/routes.yaml',
+                    'type' => 'yaml',
+                ],
+            ]);
+        }
+
+        if ($container->hasExtension('security')) {
+            $container->prependExtensionConfig('security', [
+                //'enable_authenticator_manager' => true,
+                'password_hashers' => [
+                    InMemoryUser::class => 'auto',
+                ],
+                'providers' => [
+                    'users_in_memory' => [
+                        'memory' => [],
+                    ],
+                ],
+                'firewalls' => [
+                    'dev' => [
+                        'pattern' => '^/(_(profiler|wdt)|css|images|js)/',
+                        'security' => false,
+                    ],
+                    'main' => [
+                        'lazy' => true,
+                        'provider' => 'users_in_memory',
+                    ],
+                ],
+            ]);
+        }
+
+        if ($container->hasExtension('doctrine')) {
+            $container->prependExtensionConfig('doctrine', [
+                'dbal' => [
+                    'driver' => 'pdo_sqlite',
+                    'url' => 'sqlite:///:memory:',
+                ],
+                'orm' => [
+                    'auto_generate_proxy_classes' => true,
+                    'controller_resolver' => [
+                        'auto_mapping' => false,
+                    ],
+                    'naming_strategy' => 'doctrine.orm.naming_strategy.underscore_number_aware',
+                    'auto_mapping' => true,
+                ]
+            ]);
+        }
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader): void
+    {
+        $loader->load($this->configureContainer(...));
+    }
+}
